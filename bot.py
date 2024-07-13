a409a7be31b72af4194381e9ea50328a0e132deb
IanRayburn <ianrayburn5@gmail.com>
2024-07-13 00:35:26 -0400

    feat(bot): add basic message responce to get world and get log

--- bot.py
+++ bot.py
@@ -14,6 +14,22 @@ intents = Intents.default()
 intents.message_content = True
 client = Client(intents=intents)
 
+# Check message and preform requested command
+@client.event
+async def on_message(message: Message) -> None:
+    if message.content[:5] == "$get ":
+        user_message: str = message.content[5:]
+    else:
+        return
+
+    if user_message == "world":
+        await message.channel.send("Server World File: ")
+    elif user_message == "log":
+        await message.channel.send("Server Log File: ")
+    else: 
+        await message.channel.send(f"{user_message} is invalid $get command")
+        await message.channel.send("Try: '$get world' or '$get log'")
+
 def main() -> None:
     client.run(token=TOKEN)
 
