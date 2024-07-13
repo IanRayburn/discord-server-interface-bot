bb1606a65a84ee9ae30fa1001f121dfe55a90c44
IanRayburn <ianrayburn5@gmail.com>
2024-07-12 22:07:24 -0400

    feat(settings): add CHANNEL_ID var to settings.py

--- settings.py
+++ settings.py
@@ -3,5 +3,7 @@ import os
 # .env setup
 TOKEN = os.getenv("TOKEN")
 WORLDBACKUP_LOCATION = os.get("WORLDBACKUP_LOCATION")
+CHANNEL_ID = os.get("CHANNEL_ID")
 
-# Just pass a string if enter info in this file
+# Just pass a string to TOKEN & WORLDBACKUP_LOCATION if enter info in this file
+# Make sure to pass a number to CHANNEL_ID
