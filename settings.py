5596f352c9e913a87613631b3681cafb4af7793a
IanRayburn <ianrayburn5@gmail.com>
2024-07-12 22:28:06 -0400

    fix(settings): fix problem with wrong method from os lib to access .env vars

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 import os
 
 # .env setup
-TOKEN = os.getenv("TOKEN")
-WORLDBACKUP_LOCATION = os.get("WORLDBACKUP_LOCATION")
-CHANNEL_ID = os.get("CHANNEL_ID")
+TOKEN = os.environ.get("TOKEN")
+WORLDBACKUP_LOCATION = os.environ.get("WORLDBACKUP_LOCATION")
+CHANNEL_ID = os.environ.get("CHANNEL_ID")
 
 # Just pass a string to TOKEN & WORLDBACKUP_LOCATION if enter info in this file
 # Make sure to pass a number to CHANNEL_ID
