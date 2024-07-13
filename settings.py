5e190a8d57bb573f4044757f79584c6e03a7a8ff
IanRayburn <ianrayburn5@gmail.com>
2024-07-13 07:29:07 -0400

    chore(settings): removed CHANNEL var

--- settings.py
+++ settings.py
@@ -3,11 +3,9 @@ import os
 # .env setup
 TOKEN: str = os.environ.get("TOKEN")
 WORLD_LOCATION: str = os.environ.get("WORLD_LOCATION")
-CHANNEL_ID = os.environ.get("CHANNEL_ID")
 SERVER_LOG_LOCATION: str = os.environ.get("SERVER_LOG_LOCATION")
 
 # regular setup
 BOT_LOG_LOCATION = "bot_log.txt"
 
 # Just pass a string to TOKEN & WORLDBACKUP_LOCATION if enter info in this file
-# Make sure to pass a number to CHANNEL_ID
