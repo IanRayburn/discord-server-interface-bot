6d61171aa54554324e3e3b224b5c9ecfe99ed204
IanRayburn <ianrayburn5@gmail.com>
2024-07-13 07:08:53 -0400

    feat(settings): add SERVER_LOG_LOCATION & BOT_LOG_LOCATION vars

--- settings.py
+++ settings.py
@@ -1,9 +1,13 @@
 import os
 
 # .env setup
-TOKEN = os.environ.get("TOKEN")
-WORLD_LOCATION = os.environ.get("WORLD_LOCATION")
+TOKEN: str = os.environ.get("TOKEN")
+WORLD_LOCATION: str = os.environ.get("WORLD_LOCATION")
 CHANNEL_ID = os.environ.get("CHANNEL_ID")
+SERVER_LOG_LOCATION: str = os.environ.get("SERVER_LOG_LOCATION")
+
+# regular setup
+BOT_LOG_LOCATION = "bot_log"
 
 # Just pass a string to TOKEN & WORLDBACKUP_LOCATION if enter info in this file
 # Make sure to pass a number to CHANNEL_ID
