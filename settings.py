71b638c5da31d55ecaecc75289e523f28a6c2fc7
IanRayburn <ianrayburn5@gmail.com>
2024-07-13 07:23:10 -0400

    feat(settings): change bot_log file to .txt

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ CHANNEL_ID = os.environ.get("CHANNEL_ID")
 SERVER_LOG_LOCATION: str = os.environ.get("SERVER_LOG_LOCATION")
 
 # regular setup
-BOT_LOG_LOCATION = "bot_log"
+BOT_LOG_LOCATION = "bot_log.txt"
 
 # Just pass a string to TOKEN & WORLDBACKUP_LOCATION if enter info in this file
 # Make sure to pass a number to CHANNEL_ID
