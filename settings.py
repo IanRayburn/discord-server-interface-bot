7765a08757c21a03d6150cf7c5807eee0c7a945d
IanRayburn <ianrayburn5@gmail.com>
2024-07-13 06:10:40 -0400

    fchore(setting): change WORLDBACKUP_LOCATION to WORLD_LOCATION

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ import os
 
 # .env setup
 TOKEN = os.environ.get("TOKEN")
-WORLDBACKUP_LOCATION = os.environ.get("WORLDBACKUP_LOCATION")
+WORLD_LOCATION = os.environ.get("WORLD_LOCATION")
 CHANNEL_ID = os.environ.get("CHANNEL_ID")
 
 # Just pass a string to TOKEN & WORLDBACKUP_LOCATION if enter info in this file
